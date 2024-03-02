d191062b3611069d031ea2c49f875096e79df0fc
Andres Hidalgo <andres.hidalgo@iit.it>
2024-03-02 12:37:57 +0100

    added lagarto2

--- HolaPython.py
+++ HolaPython.py
@@ -5,3 +5,6 @@ print("Hola Mundo2")
 
 lag1 = lag("Roberto", 9,2)
 print("The equivalent age is: ", lag1.computeage())
+
+lag2 = lag("Roberto", 8,3)
+print("The equivalent age is: ", lag2.computeage())
