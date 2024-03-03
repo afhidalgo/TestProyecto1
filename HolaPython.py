943f927bd066b533890df2ccdc0cfc9388a1a6e6
Andres Hidalgo <andres.hidalgo@iit.it>
2024-03-03 10:55:12 +0100

    added lagarto4

--- HolaPython.py
+++ HolaPython.py
@@ -11,3 +11,6 @@ print("The equivalent age is: ", lag2.computeage())
 
 lag3 = lag("Roberto", 7,4)
 print("The equivalent age is: ", lag3.computeage())
+
+lag4 = lag("Roberto", 6,5)
+print("The equivalent age is: ", lag4.computeage())
